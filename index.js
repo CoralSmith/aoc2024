50f696c2e9547fe185dda109a85b38ba1bece90c
Coral Smith <coral.smith92@gmail.com>
2024-12-04 01:48:01 +0000

    renaming things

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import { run as runDayOne } from "./2024/one.mjs";
-import { run as runDayTwo } from "./2024/two.mjs";
-import { run as runDayThree } from "./2024/three.mjs";
+import { run as runDayOne } from "./src/one.mjs";
+import { run as runDayTwo } from "./src/two.mjs";
+import { run as runDayThree } from "./src/three.mjs";
 
 function main() {
   console.log("Day 1");
-  runDayOne("./2024/inputs/day1.txt");
+  runDayOne("./src/inputs/day1.txt");
 
   console.log("Day 2");
-  runDayTwo("./2024/inputs/day2.txt");
+  runDayTwo("./src/inputs/day2.txt");
 
   console.log("Day 3");
-  runDayThree("./2024/inputs/day3.txt");
+  runDayThree("./src/inputs/day3.txt");
 }
 
 main();
